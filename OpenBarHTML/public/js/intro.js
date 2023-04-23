1fde2e0b15ccf335b33632f782641f4b8c0f4fcc
janvier68 <60669506+janvier68@users.noreply.github.com>
2023-04-23 03:44:59 +0200

    HTML + CSS
    j'ai tout mis dans un dossier séparait pour ne pas supprimer les autres travaux (en tout cap je ne veut pas prendre de risque)
    
    il y a tout les page html
    le css risque de changer (notament pour la page du produit)

---
+++ OpenBarHTML/public/js/intro.js
@@ -0,0 +1,188 @@
+// ! se qui se fait pour chaque fenetre
+// Get the browser window size
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+
+// if (windowHeight > windowWidth) {//tel
+    
+// }
+// else {
+//     document.getElementById("logo").style.width = "100%";
+//     // document.getElementById("logo").style.paddingTop = "30px";
+// }
+document.getElementById("logo").style.width = "60%";
+var contLogo = document.getElementById("conteneurLogo").style;
+var latete = document.getElementById("tete").style;
+
+// pour avoir un beau dégrader il faut la taill exacet
+document.getElementById("body").style.height = windowHeight + "px";
+document.getElementById("body").style.width = windowWidth + "px";
+
+
+// ! fonction 
+
+// as timePassed goes from 0 to 2000
+// left gets values from 0px to 400px
+function draw(uniter, un1, un2) {
+    latete.height = document.getElementById("tete").clientHeight - uniter + 'px';
+    document.getElementById('logo').style.height = document.getElementById('logo').clientHeight - un1 + "px";
+    document.getElementById('logo').style.width = document.getElementById('logo').clientWidth - un2 + "px";
+    // console.log(document.getElementById("conteneurLogo").clientHeight);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function att(nb,baliseafficher) {//nb en seconde
+    //cacher elm
+    baliseafficher.style.opacity=0;
+    //les position de fin
+    var heightLogoFin = document.getElementById('logo').clientHeight;
+    var widthLogoFin = document.getElementById('logo').clientWidth;
+    var heightbaniereFin = document.getElementById("tete").clientHeight;
+
+    console.log(heightLogoFin);
+    console.log(widthLogoFin);
+
+    //pos etat début
+        // fixe body
+    document.getElementById("body").style.clientHeight=window.innerHeight;
+    document.getElementById("body").style.clientWidth=window.innerWidth;
+
+    latete.height = "100%";
+    contLogo.height = "100%";
+    document.getElementById('logo').style.height = document.getElementById("body").clientHeight + "px";
+    document.getElementById('logo').style.width = document.getElementById("body").clientWidth + "px";
+
+    //calcule des fonction
+    var frameseconde = 20;
+    var delayanimation = 2000;
+    var uniterHeight2secondeBanniere = (document.getElementById("conteneurLogo").clientHeight - heightbaniereFin) / (delayanimation / frameseconde);
+    var uniterHeight2secondeLogo = (document.getElementById("body").clientHeight - heightLogoFin) / (delayanimation / frameseconde);
+    var uniterWidtht2secondeLogo = (document.getElementById("body").clientWidth - widthLogoFin) / (delayanimation / frameseconde);
+
+    for (let i = 0; i < nb; i++) {
+        console.log(`Waiting ${i} seconds...`);
+        await sleep(i * 1000);
+    }
+
+
+    //application
+    let start = Date.now(); // remember start time
+    let timer = setInterval(function () {
+        // how much time passed from the start?
+        let timePassed = Date.now() - start;
+        if (timePassed >= delayanimation) {
+        clearInterval(timer); // finish the animation after 2 seconds
+        baliseafficher.style.opacity=1;//remontrer elm
+        return;
+        }
+        // draw the animation at the moment timePassed
+        draw(uniterHeight2secondeBanniere, uniterHeight2secondeLogo, uniterWidtht2secondeLogo);
+    }, frameseconde);   
+
+}
+
+
+// function start() {
+//     //les position de fin
+//     var heightLogoFin = document.getElementById('logo').clientHeight;
+//     var widthLogoFin = document.getElementById('logo').clientWidth;
+//     var heightbaniereFin = document.getElementById("tete").clientHeight;
+
+//     console.log(heightLogoFin);
+//     console.log(widthLogoFin);
+
+//     //pos etat début
+//     latete.height = "100%";
+//     contLogo.height = "100%";
+//     document.getElementById('logo').style.height = document.getElementById("body").clientWidth + "px";
+//     document.getElementById('logo').style.width = document.getElementById("body").clientWidth + "px";
+
+//     //calcule des fonction
+//     var frameseconde = 20;
+//     var delayanimation = 2000;
+//     var uniterHeight2secondeBanniere = (document.getElementById("conteneurLogo").clientHeight - heightbaniereFin) / (delayanimation / frameseconde);
+//     var uniterHeight2secondeLogo = (document.getElementById("body").clientWidth - heightLogoFin) / (delayanimation / frameseconde);
+//     var uniterWidtht2secondeLogo = (document.getElementById("body").clientWidth - widthLogoFin) / (delayanimation / frameseconde);
+
+//     //application
+//     let start = Date.now(); // remember start time
+//     let timer = setInterval(function () {
+//         // how much time passed from the start?
+//         let timePassed = Date.now() - start;
+//         if (timePassed >= delayanimation) {
+//         clearInterval(timer); // finish the animation after 2 seconds
+//         return;
+//         }
+//         // draw the animation at the moment timePassed
+//         draw(uniterHeight2secondeBanniere, uniterHeight2secondeLogo, uniterWidtht2secondeLogo);
+//     }, frameseconde);
+// }
+
+
+
+    // scrip pour les bouton 
+function shwopsw() {
+    var x = document.getElementById("myPsw");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function shwopsw2() {
+    var x = document.getElementById("myPsw2");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+//le gris derrière
+function appgrisfond(condition) {
+    // if (condition==1) {
+        console.log(document.getElementById("conteneurPrincipal").clientHeight);
+        document.getElementById("conteneurPrincipal").style.height=
+        document.getElementById("conteneurPrincipal").clientHeight
+        -document.getElementById("endpage").clientHeight
+        -document.getElementById("boutonsumit").clientHeight
+        // window.getComputedStyle(document.getElementById('boutonsumit')).marginTop+
+        -20 //pour le margin que j'applique à chaque foit 
+        +"px";  
+    // }
+    // else
+    // {
+    //     document.getElementById("conteneurPrincipal").style.height=
+    //     document.getElementById("conteneurPrincipal").clientHeight
+    //     -document.getElementById("endpage").clientHeight
+    //     -document.getElementById("boutonsumit").clientHeight
+    //     -document.getElementById("endpagepktuexistenooooooooooon").clientHeight*2
+    //     -20 //pour le margin que j'applique à chaque foit 
+    //     +"px";  
+    // }
+    console.log(document.getElementById("conteneurPrincipal").clientHeight);
+        
+        
+}
+
+function posabsolutotalPanier(istel)
+{
+    let elem = document.getElementById("totalPayer");
+    if (istel) {//tel
+        
+        elem.style.top = document.getElementById("fortotalpayer").getBoundingClientRect().bottom
+        +"px";
+      }
+      else
+      {
+        elem.style.top = document.getElementById("fortotalpayer").getBoundingClientRect().bottom
+        -(document.getElementById("totalPayer").clientHeight/2)
+        +"px";
+        elem.style.left =document.getElementById("fortotalpayer").getBoundingClientRect().left+"px";
+      }
+}
